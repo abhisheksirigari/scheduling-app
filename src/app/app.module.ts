b1385485dcc2235bf10aefe9b3f1873628d190f5
abhisheksirigari <1289solutions@gmail.com>
2019-02-07 19:03:13 +0530

    added angular scheduler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+registerLocaleData(localeIt);
+
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
+import { SchedulerModule } from 'angular-calendar-scheduler';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { ScheduleComponent } from './components/schedule/schedule.component';
+import { SchedulerService } from 'src/app/services/scheduler.service';
+
+import { AppService } from 'src/app/services/app.service';
 
 @NgModule({
   declarations: [
@@ -11,10 +23,19 @@ import { ScheduleComponent } from './components/schedule/schedule.component';
     ScheduleComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
+    SchedulerModule.forRoot({ locale: 'en', headerDateFormat: 'daysRange' }),
   ],
-  providers: [],
+  providers: [
+    AppService,
+    { provide: LOCALE_ID, useValue: 'en-US' },
+    SchedulerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
